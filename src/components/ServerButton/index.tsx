26482d3b85ffd61c4f3148623947f34e183024e2
gabrielcancio <gabriel.cancio02@gmail.com>
2020-06-22 00:02:16 -0300

    Making server list component

--- src/components/ServerButton/index.tsx
+++ src/components/ServerButton/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import logo from '../../assets/logo.svg';
 import { Button } from './styles';
 
-const ServerButton: React.FC = () => {
-  return <div />;
+export interface IProps {
+  selected?: boolean;
+  isHome?: boolean;
+  hasNotifications?: boolean;
+  mentions?: number;
+}
+
+const ServerButton: React.FC<IProps> = ({ 
+  selected,
+  isHome,
+  hasNotifications,
+  mentions
+ }) => {
+  return (
+    <Button
+      isHome={isHome}
+      hasNotifications={hasNotifications}
+      mentions={mentions}
+      className={selected ? 'active' : ''}
+    >
+      { isHome && <img src={logo} alt="Rocketseat"/> }
+    </Button>
+  );
 }
 
 export default ServerButton;
